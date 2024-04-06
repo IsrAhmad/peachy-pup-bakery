ad7b976f1ba3462a6822a04d23eb119f21628698
IsrAhmad <Israa.099@gmail.com>
2024-04-06 15:31:45 +0200

    social icons open links + attempt at API post method
    1. No responsiveness
    2. not all issues from previous commit were solved

--- src/app/contact-us.service.ts
+++ src/app/contact-us.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactUsService {
 
-  constructor() { }
+  url = '';
+
+  constructor(private _HttpClient:HttpClient) {}
+
+  sendUserData(data:any) {
+    console.log(data)
+  }
+
+  // saveUserData(data:any) {
+  //   return this._HttpClient.post(this.url, data);
+  // }
 }
--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-
+import { ContactUsService } from '../contact-us.service';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  userData:any[] =[];
+
+  constructor(private _ContactUsService:ContactUsService) {}
+
   sendUserData(data:any) {
     console.log(data)
+    // this.userData.saveUserData(data).subscribe((result) =>{
+    //   console.log(data);
+    // })
   }
 
 }
